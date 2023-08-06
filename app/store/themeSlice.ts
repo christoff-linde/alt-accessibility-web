d1f54ef9ba34537925669a2016e85d649d662219
christoff-linde <christoff.linde@gmail.com>
2023-08-06 21:18:29 +0200

    fix: enum naming in initialState

--- app/store/themeSlice.ts
+++ app/store/themeSlice.ts
@@ -1,29 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { FontSize, LayoutOrientation } from '../types';
 
 export interface ThemeState {
   orientation: LayoutOrientation;
   fontSize: FontSize;
+  activeIndex: number;
 }
 
 const initialState: ThemeState = {
-  orientation: LayoutOrientation.right,
-  fontSize: FontSize.medium,
+  orientation: LayoutOrientation.RIGHT,
+  fontSize: FontSize.NORMAL,
+  activeIndex: 0,
 };
 
 const themeSlice = createSlice({
   name: 'theme',
   initialState,
   reducers: {
-    setOrientation(state, action: PayloadAction<LayoutOrientation>) {
-      state.orientation = action.payload;
+    setActiveIndex(state, action: PayloadAction<number>) {
+      state.activeIndex = action.payload;
     },
     setFontSize(state, action: PayloadAction<FontSize>) {
       state.fontSize = action.payload;
     },
+    setOrientation(state, action: PayloadAction<LayoutOrientation>) {
+      state.orientation = action.payload;
+    },
   },
 });
 
-export const { setOrientation, setFontSize } = themeSlice.actions;
+export const { setActiveIndex, setFontSize, setOrientation } =
+  themeSlice.actions;
 export default themeSlice.reducer;
