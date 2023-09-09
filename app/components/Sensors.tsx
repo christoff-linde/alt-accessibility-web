bcb65bf1135870e0325df2e418e4c8c3333a609d
christoff-linde <christoff.linde@gmail.com>
2023-09-09 21:49:54 +0200

    feat: add min/max recording

--- app/components/Sensors.tsx
+++ app/components/Sensors.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 'use client';
 
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import CustomButton from './Button';
 
 const initAccelerometer = (callback: Function) => {
@@ -75,6 +75,11 @@ const Sensors = () => {
   const [lightLevel, setLightLevel] = useState(0);
   const [orientation, setOrientation] = useState({ x: 0, y: 0, z: 0, w: 0 });
 
+  const [isActive, setIsActive] = useState(false);
+  const [colorString, setColorString] = useState('bg-red-600/10');
+  const [max, setMax] = useState(0);
+  const [min, setMin] = useState(0);
+
   const accelerometer = initAccelerometer(setAcceleration);
   const gyroscope = initGyroscope(setGyroAcceleration);
   const lightSensor = initAmbientLightSensor(setLightLevel);
@@ -86,7 +91,7 @@ const Sensors = () => {
     lightSensor.start();
     gyroscope.start();
     relativeOrientationSensor.start();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleSensorDeactivate = useCallback(() => {
@@ -94,12 +99,31 @@ const Sensors = () => {
     lightSensor.stop();
     gyroscope.stop();
     relativeOrientationSensor.stop();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    if (orientation.y > max) setMax(orientation.y);
+    if (orientation.y < min) setMin(orientation.y);
+  }, [orientation]);
+
+  useMemo(() => {
+    if (isActive) {
+      setColorString('bg-green-600/10 ring-green-200/10 text-green-500');
+      handleSensorActivate();
+    } else {
+      setColorString('bg-red-600/10 ring-red-200/10 text-red-500');
+      handleSensorDeactivate();
+    }
+  }, [isActive]);
 
   return (
     <div>
-      <h2 className='my-2 text-xl'>Sensor Readings</h2>
+      <h2
+        className={`my-2 rounded-md p-2 text-xl ring-1 ring-inset ${colorString}`}
+      >
+        Sensor Readings
+      </h2>
       <div className='mt-1 rounded-md p-2 ring-1 ring-cyan-200/20'>
         <p className='mb-1 ml-1 text-lg'>Accelerometer</p>
         <div className='grid w-full grid-cols-3 gap-2'>
@@ -120,33 +144,46 @@ const Sensors = () => {
           </p>
         </div>
         <p className='mb-1 ml-1 text-lg'>Gyroscope</p>
-        <div className='grid w-full grid-cols-3 gap-2'>
-          <p className='rounded-md bg-cyan-600/10 p-2 ring-1 ring-inset ring-cyan-200/10'>
+        <div className='flex w-full gap-2'>
+          <p className='w-full rounded-md bg-cyan-600/10 p-2 ring-1 ring-inset ring-cyan-200/10'>
             {gyroAcceleration.x.toPrecision(3)}
           </p>
-          <p className='rounded-md bg-cyan-600/10 p-2 ring-1 ring-inset ring-cyan-200/10'>
+          <p className='w-full rounded-md bg-cyan-600/10 p-2 ring-1 ring-inset ring-cyan-200/10'>
             {gyroAcceleration.y.toPrecision(3)}
           </p>
-          <p className='rounded-md bg-cyan-600/10 p-2 ring-1 ring-inset ring-cyan-200/10'>
+          <p className='w-full rounded-md bg-cyan-600/10 p-2 ring-1 ring-inset ring-cyan-200/10'>
             {gyroAcceleration.z.toPrecision(3)}
           </p>
         </div>
         <p className='mb-1 ml-1 text-lg'>Relative Orientation</p>
-        <div className='grid w-full grid-cols-3 gap-2'>
-          <p className='rounded-md bg-cyan-600/10 p-2 ring-1 ring-inset ring-cyan-200/10'>
-            {orientation.x.toPrecision(3)}
+        <div className='flex w-full gap-2'>
+          <p className='w-full rounded-md bg-cyan-600/10 p-2 ring-1 ring-inset ring-cyan-200/10'>
+            {orientation.x.toPrecision(2)}
           </p>
-          <p className='rounded-md bg-red-600/10 p-2 ring-1 ring-inset ring-red-200/10'>
+          <p
+            className={`w-full rounded-md p-2 ring-1 ring-inset ${colorString} font-bold`}
+          >
             {orientation.y.toPrecision(3)}
           </p>
-          <p className='rounded-md bg-cyan-600/10 p-2 ring-1 ring-inset ring-cyan-200/10'>
-            {orientation.z.toPrecision(3)}
+          <p className='w-full rounded-md bg-cyan-600/10 p-2 ring-1 ring-inset ring-cyan-200/10'>
+            {orientation.z.toPrecision(2)}
+          </p>
+        </div>
+        <p className='mb-1 ml-1 text-lg'>Min / Max Orientation</p>
+        <div className='flex w-full gap-2'>
+          <p className='w-full rounded-md bg-cyan-600/10 p-2 ring-1 ring-inset ring-cyan-200/10'>
+            {min.toPrecision(3)}
+          </p>
+          <p className='w-full rounded-md bg-cyan-600/10 p-2 ring-1 ring-inset ring-cyan-200/10'>
+            {max.toPrecision(3)}
           </p>
         </div>
       </div>
       <div className='mt-4 flex gap-4'>
-        <CustomButton onClick={handleSensorActivate}>Activate</CustomButton>
-        <CustomButton onClick={handleSensorDeactivate}>Deactivate</CustomButton>
+        <CustomButton onClick={() => setIsActive(true)}>Activate</CustomButton>
+        <CustomButton onClick={() => setIsActive(false)}>
+          Deactivate
+        </CustomButton>
       </div>
     </div>
   );
